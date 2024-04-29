6a2a00ba694630ed74c9e433d3456377f286bca5
Ushitora Anqou <ushitora@anqou.net>
2024-04-29 17:48:09 +0900

    use ocamlc instead of ocamlopt for better compilation time

--- lib/executor.ml
+++ lib/executor.ml
@@ -19,19 +19,19 @@ let rm_rf dir_name =
 
 let main_ml_file_name = "main.ml"
 let main_exe_file_name = "main.exe"
-let ocamlopt_stdout_file_name = "ocamlopt.stdout"
-let ocamlopt_stderr_file_name = "ocamlopt.stderr"
+let ocamlc_stdout_file_name = "ocamlc.stdout"
+let ocamlc_stderr_file_name = "ocamlc.stderr"
 let main_exe_stdout_file_name = "main.exe.stdout"
 let main_exe_stderr_file_name = "main.exe.stderr"
 
-let execute' ~dir_name ~ocamlopt_path ~redirect ast =
+let execute' ~dir_name ~ocamlc_path ~redirect ast =
   let main_ml = Filename.concat dir_name main_ml_file_name in
   let main_exe = Filename.concat dir_name main_exe_file_name in
-  let redirect_ocamlopt =
+  let redirect_ocamlc =
     if redirect then
       Printf.sprintf "> %s 2> %s"
-        (Filename.quote (Filename.concat dir_name ocamlopt_stdout_file_name))
-        (Filename.quote (Filename.concat dir_name ocamlopt_stderr_file_name))
+        (Filename.quote (Filename.concat dir_name ocamlc_stdout_file_name))
+        (Filename.quote (Filename.concat dir_name ocamlc_stderr_file_name))
     else ""
   in
   let redirect_main_exe =
@@ -49,14 +49,12 @@ let execute' ~dir_name ~ocamlopt_path ~redirect ast =
       Pprintast.structure f ast;
       Format.pp_print_flush f ());
   (match
-     let com =
-       Filename.quote_command ocamlopt_path [ "-o"; main_exe; main_ml ]
-     in
-     let com = com ^ redirect_ocamlopt in
+     let com = Filename.quote_command ocamlc_path [ "-o"; main_exe; main_ml ] in
+     let com = com ^ redirect_ocamlc in
      Unix.system com
    with
   | WEXITED 0 -> ()
-  | _ -> failwith "ocamlopt failed");
+  | _ -> failwith "ocamlc failed");
   (match
      let com = Filename.quote_command main_exe [] in
      let com = com ^ redirect_main_exe in
@@ -73,7 +71,7 @@ let execute ?(remove_tmp_dir = true) ast =
   let dir_name = Filename.temp_dir ?temp_dir "jitsonnet" "" in
   Fun.protect ~finally:(fun () -> if remove_tmp_dir then rm_rf dir_name)
   @@ fun () ->
-  execute' ~dir_name ~ocamlopt_path:"ocamlopt" ~redirect:true ast;
+  execute' ~dir_name ~ocamlc_path:"ocamlc" ~redirect:true ast;
   let ic = open_in_bin (Filename.concat dir_name main_exe_stdout_file_name) in
   Fun.protect ~finally:(fun () -> close_in ic) @@ fun () ->
   In_channel.input_all ic
@@ -81,5 +79,5 @@ let execute ?(remove_tmp_dir = true) ast =
 let execute_from_cli ast =
   let dir_name = Filename.temp_dir "jitsonnet" "" in
   Fun.protect ~finally:(fun () -> rm_rf dir_name) @@ fun () ->
-  execute' ~dir_name ~ocamlopt_path:"ocamlopt" ~redirect:false ast;
+  execute' ~dir_name ~ocamlc_path:"ocamlc" ~redirect:false ast;
   ()
